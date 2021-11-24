8d29b2d0616c8f95ef902ba6738a37d87c94ca40
Vidar Erkendal <Vidar.erkendal@gmail.com>
2021-11-24 14:05:10 +0100

    början

---
+++ VinterProjektet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VinterProjektet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gaming time");
+        }
+    }
+}
